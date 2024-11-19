43e11d600cb6f54cdc3891b59f1416c695dc2fd8
Danil <chorntttt@gmail.com>
2024-11-19 22:03:12 +0300

    added config yml

--- config/config.go
+++ config/config.go
@@ -8,31 +8,31 @@ import (
 )
 
 type Config struct {
-	App   App   
-	HTTP  HTTP  
-	DB    DB   
-	Kafka Kafka 
+	App   App   `yaml:"app"`
+	HTTP  HTTP  `yaml:"http"`
+	DB    DB    `yaml:"postgres"`
+	Kafka Kafka `yaml:"kafka"`
 }
 type App struct {
-	Name    string 
-	Version string 
+	Name    string `yaml:"name" env-required:"true" env:"APP_NAME"`
+	Version string `yaml:"version" env-required:"true" env:"APP_VERSION"`
 }
 type HTTP struct {
-	Host string 
-	Port string 
+	Host string `yaml:"host" env-requiered:"true" env:"HTTP_HOST"`
+	Port string `yaml:"port" env-required:"true" env:"HTTP_PORT"`
 }
 type DB struct {
-	Host     string 
-	Port     string 
-	User     string 
-	Password string 
-	DBName   string 
-	PgDriver string 
-	Schema   string 
+	Host     string `yaml:"host" env-requiered:"true" env:"PG_HOST"`
+	Port     string `yaml:"port" env-required:"true" env:"PG_PORT"`
+	User     string `yaml:"user" env-required:"true" env:"PG_USER"`
+	Password string `yaml:"password" env-required:"true" env:"PG_PASSWORD"`
+	DBName   string `yaml:"name" env:"PG_NAME" env-required:"true" `
+	PgDriver string `yaml:"pg_driver" env:"PG_PG_DRIVER" env-required:"true" `
+	Schema   string `yaml:"schema" env:"PG_SCHEMA" env-required:"true" `
 }
 type Kafka struct {
-	BootstrapServers string 
-	Topic            string 
+	BootstrapServers string `env-required:"true" yaml:"bootstrap_servers" env:"KAFKA_BOOTSTRAP_SERVERS"`
+	Topic            string `yaml:"topic" env-required:"true" env:"KAFKA_TOPIC"`
 }
 
 func NewConfig() (*Config, error) {
