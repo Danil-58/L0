e4c4c166e269843dbefbc8214ba4ffe2843f4811
Danil <chorntttt@gmail.com>
2024-11-20 14:00:09 +0300

    added app

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,7 +2,11 @@ package app
 
 import (
 	"L0/config"
+	"L0/internal/cache"
+	"L0/internal/controller"
 	"L0/internal/database"
+	"L0/internal/generate"
+	"L0/internal/kafka"
 	"context"
 	"errors"
 	"fmt"
@@ -11,6 +15,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -35,20 +40,57 @@ func (s *Server) ListenAndServe(orderHand echo.HandlerFunc, allOrdersHand echo.H
 }
 
 func Run(cfg *config.Config) {
-
+	// Создаем продюсера (Kafka)
+	producer, err := kafka.NewProducer(&cfg.Kafka)
+	if err != nil {
+		log.Fatalf("failed to create Kafka producer: %v", err)
+	}
+	// Создаем консьюмера (Kafka)
+	consumer, err := kafka.NewConsumer(&cfg.Kafka)
+	if err != nil {
+		log.Fatalf("failed to create Kafka consumer: %v", err)
+	}
+	// Подключаемся к PostgreSQL
 	conn, err := database.Connect(&cfg.DB)
 	if err != nil {
 		log.Fatalf("error connecting to PostgreSQL: %v", err)
 	}
 	defer conn.Close()
 	db := database.NewDB(conn)
-
+	// Создаем таблицу, если ее нет
 	err = db.CreateTable()
 	if err != nil {
 		log.Fatalf("error creating table: %v", err)
 	}
-
-
+	// Инициализируем кэш
+	cache := cache.NewCache(db)
+	cache.Preload()
+	// Запускаем горутину для публикации заказов
+	go func() {
+		for {
+			order := generate.GetOrder()
+			fmt.Println("Order is sent")
+			err := producer.Publish(*order)
+			if err != nil {
+				log.Printf("error publishing: %v\n", err)
+			}
+			time.Sleep(30 * time.Second)
+		}
+	}()
+	// Запускаем горутину для подписки на заказы
+	go func() {
+		for {
+			order, err := consumer.Subscribe()
+			if err != nil {
+				log.Printf("error subscribing: %v\n", err)
+				continue
+			}
+			fmt.Println("Order received")
+			cache.AddCache(*order)
+			time.Sleep(30 * time.Second)
+		}
+	}()
+	// Создаем HTTP-Server
 	httpServer := NewServer()
 	apiController := controller.NewOrderController(cache)
 
@@ -61,5 +103,13 @@ func Run(cfg *config.Config) {
 			log.Printf("HTTP server: %v", err)
 		}
 	}()
+
+	<-ctx.Done()
+	log.Print("Graceful shutdown")
+
+	// Корректное завершение работы сервера (Graceful shutdown)
+	if err := httpServer.echo.Shutdown(ctx); err != nil {
+		httpServer.echo.Logger.Fatal(err)
+	}
 
 }
